f1f6c198db69413244f9e248739d8ad4016c38b8
raresluke98 <rares.luke98@gmail.com>
2022-02-01 23:32:36 +0200

    update admin

--- src/app/admin/admin.component.ts
+++ src/app/admin/admin.component.ts
@@ -1,24 +1,21 @@
-import { Component, OnInit } from '@angular/core';
+﻿import { Component, OnInit } from '@angular/core';
 import { first } from 'rxjs/operators';
 
 import { User } from '@app/_models';
 import { UserService } from '@app/_services';
 
-@Component({
-  templateUrl: 'admin.component.html'
-})
+@Component({ templateUrl: 'admin.component.html' })
 export class AdminComponent implements OnInit {
-  loading = false;
-  users: User[] = [];
+    loading = false;
+    users: User[] = [];
 
-  constructor(private userService: UserService) { }
+    constructor(private userService: UserService) { }
 
-  ngOnInit(){
-    this.loading = true;
-    this.userService.getAll().pipe(first()).subscribe(users => {
-      this.loading = false;
-      this.users = users;
-    });
-  }
-
+    ngOnInit() {
+        this.loading = true;
+        this.userService.getAll().pipe(first()).subscribe(users => {
+            this.loading = false;
+            this.users = users;
+        });
+    }
 }
