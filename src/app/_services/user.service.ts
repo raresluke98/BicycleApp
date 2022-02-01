f79c129a3e2a4111407d34f6acd0aad6deb54b66
raresluke98 <rares.luke98@gmail.com>
2022-02-01 23:32:17 +0200

    update services

--- src/app/_services/user.service.ts
+++ src/app/_services/user.service.ts
@@ -1,21 +1,18 @@
-import { Injectable } from '@angular/core';
+﻿import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
 import { environment } from '@environments/environment';
-import { User } from '@app/_models'
+import { User } from '@app/_models';
 
-@Injectable({
-  providedIn: 'root'
-})
+@Injectable({ providedIn: 'root' })
 export class UserService {
+    constructor(private http: HttpClient) { }
 
-  constructor(private http: HttpClient) { }
+    getAll() {
+        return this.http.get<User[]>(`${environment.apiUrl}/users`);
+    }
 
-  getAll() {
-    return this.http.get<User[]>(`${environment.apiUrl}/users`);
-  }
-
-  getById(id: number) {
-    return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
-  }
+    getById(id: number) {
+        return this.http.get<User>(`${environment.apiUrl}/users/${id}`);
+    }
 }
