0d6c41caf62996b3b31a1e0b77ee6bc4a98100d1
raresluke98 <rares.luke98@gmail.com>
2022-02-01 20:01:50 +0200

    added auth.guard

---
+++ src/app/_helpers/auth.guard.ts
@@ -0,0 +1,33 @@
+import { Injectable } from '@angular/core';
+import { ActivatedRouteSnapshot, CanActivate, Router, RouterStateSnapshot } from '@angular/router';
+
+import { AuthenticationService } from '@app/_services'
+
+@Injectable({
+    providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) { }
+
+    canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot) {
+        const user = this.authenticationService.userValue;
+        if (user) {
+            // check if route is restricted by role
+            // TODO: see if error disappeared (.roles)
+            if (route.data.roles && route.data.roles.indexOf(user.role) === -1) {
+                // role not authorised, redirect to home page
+                this.router.navigate(['/']);
+                return false;
+            }
+
+            // auth, so return true
+            return true;
+        }
+        // not logged in, redirect to login page
+        this.router.navigate(['/login'], { queryParams: { returnUrl: state.url } });
+        return false;
+    }
+}
